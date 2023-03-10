c0753339a081ba3869932301a48b303feb119c13
kkthmyh <kkth.xj@gmail.com>
2023-03-10 18:19:17 +0800

    fix

--- p14_if_let/src/main.rs
+++ p14_if_let/src/main.rs
@@ -1,18 +1,16 @@
 // if let 处理只关心一种匹配而忽略其他匹配的情况
 
 fn main() {
-
     let num = Some(0);
     // 假设当前我们只处理Some(3)的情况,可以使用if let代替
     match num {
         Some(3) => println!("3333"),
-        _ =>println!("others"),
+        _ => println!("others"),
     }
 
-    if let Some(3) = v{
+    if let Some(3) = num {
         println!("3333");
-    }else {
+    } else {
         println!("others");
     }
-
 }
